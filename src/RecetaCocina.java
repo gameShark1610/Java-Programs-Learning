994c789becd0fe39718cb8f1d5eb0980cae1b8a7
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-15 13:13:06 -0600

    Scanner: Receta de cocina

---
+++ src/RecetaCocina.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RecetaCocina {
+    public static void main(String[] args) {
+        var consola= new Scanner(System.in);
+        System.out.println("***Recetas de Cocina");
+
+        System.out.print("Ingresa el nombre: ");
+        var nombre= consola.nextLine();
+
+        System.out.print("Ingresa los ingredientes: ");
+        var ingredientes= consola.nextLine();
+
+        System.out.print("Ingresa el tiempo de preparacion (min): ");
+        var tiempo= consola.nextInt();
+
+        System.out.print("Ingresa la dificultad: " + consola.nextLine());
+        var dificultad= consola.nextLine();
+
+        System.out.println("Nombre receta: " + nombre);
+        System.out.println("Ingredientes: " + ingredientes);
+        System.out.println("Tiempo de preparacion: " + tiempo);
+        System.out.println("Dificultad: " + dificultad);
+
+
+
+    }
+}
