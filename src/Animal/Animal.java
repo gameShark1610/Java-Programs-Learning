6c8bcd92fddaf1210e8c12380a761a42e5a27046
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-16 11:14:25 -0600

    Herencia Parte 2

--- src/Animal/Animal.java
+++ src/Animal/Animal.java
@@ -15,3 +15,20 @@ class Perro extends Animal{
         System.out.println("Puedo ladrar");
     }
 }
+
+class pruebaAnimal{
+    public static void main(String[] args) {
+        System.out.println("*** Ejemplo de Herencia ***");
+        System.out.println("Clase Padre, soy un Animal");
+        var animal1=new Animal();
+        animal1.comer();
+        animal1.dormir();
+        //animal1.hacerSonido(); // Este metodo no existe en la clase padre
+
+        System.out.println("\nClase Hija, soy un Perro");
+        var perro1 = new Perro();
+        perro1.comer();
+        perro1.dormir();
+        perro1.hacerSonido();
+    }
+}
