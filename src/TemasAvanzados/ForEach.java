7f6dd2ab8de9fee2914b4a004b31821d68a63521
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-16 14:15:22 -0600

    ForEach

--- src/TemasAvanzados/ForEach.java
+++ src/TemasAvanzados/ForEach.java
@@ -1,4 +1,12 @@
 package TemasAvanzados;
 
 public class ForEach {
+    public static void main(String[] args) {
+        int edades[] = {5,10,30,45};
+
+        // foreach
+        for (int edad : edades){
+            System.out.println("edad = " + edad);
+        }
+    }
 }
