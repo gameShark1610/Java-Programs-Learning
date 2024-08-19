a8643380f8446fca18ed3baca7e8413c59738ac2
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-18 20:33:56 -0600

    Interfaces

---
+++ src/TemasAvanzados/InterfacesTraductor.java
@@ -0,0 +1,41 @@
+package TemasAvanzados;
+
+public interface InterfacesTraductor {
+    // public y abstract
+    void traducir();
+
+    // Metodos con implementacion por default
+    default void iniciarTraductor(){
+        System.out.println("Iniciando traductor...");
+    }
+}
+
+class Ingles implements InterfacesTraductor{
+    @Override
+    public void traducir(){
+        System.out.println("Traduzco a Ingles");
+    }
+}
+
+class Frances implements InterfacesTraductor{
+    public void traducir(){
+        System.out.println("Traduzco a Frances");
+    }
+
+    @Override
+    public void iniciarTraductor(){
+        System.out.println("Iniciando traductor en Frances...");
+    }
+}
+
+class PruebaTraductor{
+    public static void main(String[] args) {
+        InterfacesTraductor ingles = new Ingles();
+        ingles.iniciarTraductor();
+        ingles.traducir();
+        // Traductor en Frances
+        InterfacesTraductor frances = new Frances();
+        frances.iniciarTraductor();
+        frances.traducir();
+    }
+}
