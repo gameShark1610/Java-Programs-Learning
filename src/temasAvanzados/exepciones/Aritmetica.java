b754823246c571997362dc91158398716690b170
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-18 22:40:00 -0600

    Finally y Exeption

--- src/temasAvanzados/exepciones/Aritmetica.java
+++ src/temasAvanzados/exepciones/Aritmetica.java
@@ -1,9 +1,9 @@
 package temasAvanzados.exepciones;
 
 public class Aritmetica {
-    public static int division(int numerador, int denominador){
+    public static int division(int numerador, int denominador) throws  Exception{
         if (denominador==0)
-            throw new RuntimeException("Division entre Cero");
+            throw new Exception("Division entre Cero");
         return numerador/denominador;
     }
 }
--- src/temasAvanzados/exepciones/PruebaAritmetica.java
+++ src/temasAvanzados/exepciones/PruebaAritmetica.java
@@ -3,10 +3,13 @@ package temasAvanzados.exepciones;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         try {
-            var resultado=Aritmetica.division(10,0);
+            var resultado=Aritmetica.division(10,2);
             System.out.println("resultado = " + resultado);
         } catch (Exception e) {
             System.out.println("Ocurrio un error : "+ e);
+        }Ex
+        finally {
+            System.out.println("Se reviso la division entre cero");
         }
 
     }
