e3714ac61446ce13b48a2c57726c78b160c505c0
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-18 21:26:39 -0600

    JavaBeans

---
+++ src/TemasAvanzados/JavaBeans.java
@@ -0,0 +1,45 @@
+package TemasAvanzados;
+
+import java.io.Serializable;
+
+public class JavaBeans {
+    public static void main(String[] args) {
+        var persona = new Persona();
+        persona.setNombre("Karla");
+        persona.setApellido("Lara");
+        System.out.println("persona = " + persona);
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Apellido: " + persona.getApellido());
+    }
+}
+
+class Persona implements Serializable {
+    private String nombre;
+    private String apellido;
+
+    public Persona(){}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+}
