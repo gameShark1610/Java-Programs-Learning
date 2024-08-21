15ec7b1913d892850900c298f23eeab32c7c85b3
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-20 21:01:16 -0600

    Listas de Java

---
+++ src/temasAvanzados/Listas.java
@@ -0,0 +1,23 @@
+package temasAvanzados;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listas {
+    public static void main(String[] args) {
+        List miLista = new ArrayList();
+        miLista.add("Lunes");
+        miLista.add("Martes");
+        miLista.add("Miercoles");
+        miLista.add("Jueves");
+        miLista.add("Viernes");
+        miLista.add("Sabado");
+        miLista.add("Domingo");
+        miLista.add("Domingo");
+
+        for (Object elemento: miLista){
+            System.out.println(elemento);
+        }
+
+    }
+}
