5d78a8c91374964aecb128f619abe2c0728c21e8
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-21 17:42:56 -0600

    Maquina Snacks 4

--- src/maquinaSnacks/MaquinaSnacks.java
+++ src/maquinaSnacks/MaquinaSnacks.java
@@ -19,7 +19,7 @@ public class MaquinaSnacks {
         while (!salir){
             try {
                 var opcion = mostrarMenu(consola);
-                //salir = ejecutarOpciones(opcion,consola,productos);
+                salir = ejecutarOpciones(opcion,consola,productos);
 
             } catch (Exception e){
                 System.out.println("Ocurrio un error: " + e);
@@ -40,6 +40,33 @@ public class MaquinaSnacks {
                 Elige una opcion:\s""");
         // Leemos y retornamos la opcion seleccionada por el usuario
         return  Integer.parseInt(consola.nextLine());
+    }
+
+    private  static  boolean ejecutarOpciones(int opcion, Scanner consola, List<Snack> productos){
+        var salir= false;
+        switch (opcion){
+            case 1 -> comprarSnack(consola, productos);
+        }
+        return salir;
+    }
+
+    private static void comprarSnack(Scanner consola, List<Snack> productos) {
+        System.out.print("Que snack quieres comprar (id)? ");
+        var idSnack=Integer.parseInt(consola.nextLine());
+        //Validar que el snack existe en la lista de snacks
+        var snackEncontrado = false;
+        for (var snack: Snacks.getSnacks()){
+            if(idSnack == snack.getIdSnack()){
+                //Agregamos el snack a la lista de productos
+                productos.add(snack);
+                System.out.println("Ok, Snack agregado: " + snack);
+                snackEncontrado = true;
+                break;
+            }
+        }
+        if (!snackEncontrado){
+            System.out.println("Id de snack no encontrado: " + idSnack);
+        }
     }
 
 
