489d04dd760186ab4ebb5f256c68a37ad9ac6260
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2024-08-24 14:58:27 -0600

    Crear archivos

---
+++ src/archivos/CrearArchivo.java
@@ -0,0 +1,28 @@
+package archivos;
+
+import java.io.File;//Input-output
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CrearArchivo {
+    public static void main(String[] args) {
+        var nombreArchivo = "mi_archivo.txt";
+        var archivo = new File(nombreArchivo);
+        try {
+        if (archivo.exists()){
+            System.out.println("El archivo ya existe");
+        }
+        else {
+            //Creamos el archivo
+
+                var salida = new PrintWriter(new FileWriter(archivo));
+                salida.close();
+                System.out.println("Se ha creado el archivo");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al crear archivo: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
